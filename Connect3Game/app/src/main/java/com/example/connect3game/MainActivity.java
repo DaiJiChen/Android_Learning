96af82c316086819e4f6cf10734499877a2c73bd
DaiJichen <1245487099@qq.com>
2020-12-14 23:49:06 -0500

    Toast result on Screen, and set gameActive = false;

--- Connect3Game/app/src/main/java/com/example/connect3game/MainActivity.java
+++ Connect3Game/app/src/main/java/com/example/connect3game/MainActivity.java
@@ -7,14 +7,18 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private boolean redTurn = true;
+    boolean gameActive = true;
 
     //red:1, yellow: -1, empty: 0
     private int[] gameState = {0,0,0,0,0,0,0,0,0};
 
     public void click(View view) {
+        if(!gameActive) return;
+
         ImageView image = (ImageView) view;
         int tag = Integer.valueOf(image.getTag().toString());
 
@@ -42,13 +46,21 @@ public class MainActivity extends AppCompatActivity {
             redTurn = true;
         }
 
-        int result = checkResult();
+        int result = calResult();
         Log.i("info", "result: "+result);
+        if(result == 1) {
+            Toast.makeText(this, "Red has won", Toast.LENGTH_LONG).show();
+            gameActive = false;
+        }
+        else if(result == -1) {
+            Toast.makeText(this, "Yellow has won", Toast.LENGTH_LONG).show();
+            gameActive = false;
+        }
 
     }
 
-    //return 1: red win, -1, yellow win, 0 no one win, 2 both win
-    private int checkResult() {
+    //return 1: red win, -1, yellow win, 0 no one win
+    private int calResult() {
         int[][] winningPosition = {{0,1,2}, {3,4,5}, {6,7,8}, {0,3,6}, {1,4,7}, {2,5,8}, {0,4,8}, {2,4,6}};
         boolean redWin = false;
         boolean yellowWin = false;
@@ -61,7 +73,6 @@ public class MainActivity extends AppCompatActivity {
 
         if(redWin && !yellowWin) return 1;
         else if(!redWin && yellowWin) return -1;
-        else if(redWin && yellowWin) return 2;
         else return 0;
     }
 
