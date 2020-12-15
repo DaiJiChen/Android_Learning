876d2c478416f05aa88d8e542e763f47e7f5d6bf
DaiJichen <1245487099@qq.com>
2020-12-15 03:58:27 -0500

    Audio Demo

---
+++ UdacityAndroidMid/BasicPhrises/app/src/main/java/com/example/basicphrises/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.basicphrises;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+//    public void playPhrase(View view) {
+//        Button button = (Button) view;
+//
+//        Log.i("Button Pressed", button.getTag().toString());
+//        String nameOfFile = button.getTag().toString();
+//        //MediaPlayer mediaPlayer = MediaPlayer.create(this, getResources().getIdentifier(nameOfFile, "raw", getPackageName()));
+//        MediaPlayer mediaPlayer = MediaPlayer.create(this, getResources().getIdentifier(button.getTag().toString(), "raw", getPackageName()));
+//        mediaPlayer.start();
+//    }
+
+    public void playPhrase(View view) {
+
+        Button buttonPressed = (Button) view;
+
+        Log.i("Button pressed", buttonPressed.getTag().toString());
+
+        MediaPlayer mediaPlayer = MediaPlayer.create(this, getResources().getIdentifier(buttonPressed.getTag().toString(), "raw", getPackageName()));
+
+        mediaPlayer.start();
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
