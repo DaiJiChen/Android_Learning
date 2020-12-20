6ce157bd679bfdf0ede1f177c6a0cf7665265683
DaiJichen <1245487099@qq.com>
2020-12-19 23:07:39 -0500

    fixed infinite number

--- UdacityAndroidMid/app/src/main/java/com/example/timestables/MainActivity.java
+++ UdacityAndroidMid/app/src/main/java/com/example/timestables/MainActivity.java
@@ -3,6 +3,12 @@ package com.example.timestables;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SeekBar;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,40 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView listView = findViewById(R.id.listView);
+
+        ArrayList<Integer> array = new ArrayList<>();
+        for(int i = 0; i < 10; i++)
+            array.add(i+1);
+
+        ArrayAdapter<Integer> arrayAdapter = new ArrayAdapter<Integer>(this, android.R.layout.simple_list_item_1, array);
+
+        listView.setAdapter(arrayAdapter);
+
+        SeekBar seekBar = findViewById(R.id.seekBar);
+        seekBar.setMax(20);
+        seekBar.setProgress(1);
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                Log.i("SeekBar changed", "ratio = " + i);
+
+                if(i < 1) i = 1;
+
+                for(int index = 0; index < array.size(); index++)
+                    array.set(index, (index+1) * i);
+                
+                arrayAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) { }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) { }
+        });
+
+
     }
 }
