6a13eec6912c009d302b4cd49065e236bc21a70a
DaiJichen <1245487099@qq.com>
2020-12-20 00:10:50 -0500

    configure seekbar onChangeListener

--- EggTimer/app/src/main/java/com/example/eggtimer/MainActivity.java
+++ EggTimer/app/src/main/java/com/example/eggtimer/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.eggtimer;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,36 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SeekBar seekBar = findViewById(R.id.seekBar);
+        TextView textView = findViewById(R.id.textView);
+
+        seekBar.setMax(600);
+        seekBar.setProgress(30);
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                String secString = String.valueOf(i % 60);
+                if((i % 60 < 10)) secString = "0" + secString;
+
+                String minString = String.valueOf(i / 60);
+                if((i / 60 < 10)) minString = "0" + minString;
+
+                textView.setText(minString + ":" + secString);
+                Log.i("seekBar changed", "Curr time is: " + minString + ":" + secString);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
     }
 }
